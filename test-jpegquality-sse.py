9ac000f379592dd6b7d7953993852a00090c0fed
bitsgalore <johan.vanderknijff@kb.nl>
2024-10-26 01:20:10 +0200

    added script for alternative quality estimation based directly on standard jpeg q tables

--- test-jpegquality-sse.py
+++ test-jpegquality-sse.py
@@ -24,11 +24,6 @@ def parseCommandLine():
                         type=str,
                         nargs='+',
                         help="input JPEG(s) (wildcards allowed)")
-    parser.add_argument('--verbose',
-                        action="store_true",
-                        help="print variable values at each iteration",
-                        dest="verboseFlag",
-                        default=False)
 
     # Parse arguments
     args = parser.parse_args()
@@ -37,9 +32,17 @@ def parseCommandLine():
 
 
 def computeJPEGQuality(image):
-    """Returns JPEG quality and exactness flag of a JPEG image based on
-    minimum sum of squared errors between quantization tables and standard
-    JPEG quantization tables"""
+    """Returns JPEG quality based on best correspondence between image
+    quantization tables and standard tables from Annex K in ISO/IEC 10918-1.
+    
+    The image quantization tables are compared against standard quantization
+    tables for *all* possible quality levels, which are generated using
+    Equations 1 and 2 in Kornblum (2008):
+
+    https://www.sciencedirect.com/science/article/pii/S1742287608000285
+
+    (Also explained in: https://stackoverflow.com/a/29216609/1209004)
+    """
 
     # Standard JPEG luminance and chrominance quantization tables
     # for 50% quality (ISO/IEC 10918-1 : 1993(E)), Annex K
@@ -52,7 +55,6 @@ def computeJPEGQuality(image):
                 95,  98, 103, 104, 103,  62,  77, 113,
                 121,112, 100, 120,  92, 101, 103, 99]
 
-
     chrom_base = [17,  18,  18,  24,  21,  24,  47,  26,
                   26,  47,  99,  66,  56,  66,  99,  99,
                   99,  99,  99,  99,  99,  99,  99,  99,
@@ -62,38 +64,7 @@ def computeJPEGQuality(image):
                   99,  99,  99,  99,  99,  99,  99,  99,
                   99,  99,  99,  99,  99,  99,  99,  99]
 
-    # Generate standard quantization tables for all possible quality levels
-    # See https://stackoverflow.com/a/29216609/1209004, which is based on
-    # Kornblum (2008)
-
-    lum_all = {}
-    chrom_all = {}
-
-    for i in range(100):
-        lum = array.array('I')
-        chrom = array.array('I')
-        # Quality level
-        Q = i+1
-        # Scaling factor
-        if Q < 50:
-            S = 5000/Q
-        else:
-            S = 200 - 2*Q
-
-        # Compute all table values from scaling factor
-        for Tbl in lum_base:
-            Tsl = max(math.floor((S*Tbl + 50) / 100), 1)
-            lum.append(Tsl)
-
-        for Tbc in chrom_base:
-            Tsc = max(math.floor((S*Tbc + 50) / 100), 1)
-            chrom.append(Tsc)
-
-        # Add arrays to dictionary
-        lum_all[i] = lum
-        chrom_all[i] = chrom
-
-    # Quantization tables of image
+    # Image quantization tables
     qdict = image.quantization
     noTables = len(qdict)
 
@@ -102,37 +73,57 @@ def computeJPEGQuality(image):
 
     # Iterate over all quality levels
     for i in range(100):
-        # Iterate over all values in standard quantization tables for this
-        # quality level, and compute sum of squared differences relative to
-        # actual values in image quantization tables
-        sumSqErrors = 0
-        for j in range(len(qdict[0])):
-            #print(j, qdict[0][j], lum_all[i][j], qdict[1][j], chrom_all[i][j])
-            # Luminance quantization table
-            sumSqErrors += (qdict[0][j] - lum_all[i][j])**2
+        # Quality level
+        Q = i+1
+        # Scaling factor
+        if Q < 50:
+            S = 5000/Q
+        else:
+            S = 200 - 2*Q
+
+        # Initialize sum of squared differences, with is used to characterize
+        # correspondence between image and standard q tables for each quality
+        sumSqDiffs = 0
+
+        # Iterate over all values in quantization tables for this quality
+        for j in range(64):
+            # Compute standard luminance table value from scaling factor
+            Tslum = min(max(math.floor((S*lum_base[j] + 50) / 100), 1), 255)
+            # Update sum of squared differences relative to corrsponding
+            # image table value
+            sumSqDiffs += (qdict[0][j] - Tslum)**2
+
             if noTables >= 2:
-                # Chrominance quantization table
-                sumSqErrors  += (qdict[1][j] - chrom_all[i][j])**2
+                # Compute standard chrominance table value from scaling factor
+                Tschrom = min(max(math.floor((S*chrom_base[j] + 50) / 100), 1), 255)
+                # Update sum of squared differences relative to corrsponding
+                # image table value
+                sumSqDiffs  += (qdict[1][j] - Tschrom)**2
 
-        errors.append(sumSqErrors)
+            j += 1
 
+        errors.append(sumSqDiffs)
+
+    # Quality is estimated as level with smallest sum of squared differences
     qualityEst = errors.index(min(errors)) + 1
-    sumSqErrors = min(errors)
-    return qualityEst, sumSqErrors
+    # Sum of squared differences value of 0 indicates exact match with standard
+    # JPEG quantization tables; any other value means non-standard tables were used
+    # and estimate is an approximation
+    sumSqDiffs = min(errors)
+    return qualityEst, sumSqDiffs
 
 def main():
     args = parseCommandLine()
     myJPEGs =  args.JPEGsIn
     myJPEGs.sort()
-    verboseFlag = args.verboseFlag
 
     for JPEG in myJPEGs:
         with open(JPEG, 'rb') as fIn:
             im = Image.open(fIn)
             im.load()
             print("*** Image {}:".format(JPEG))
-            quality, sumSqErrors = computeJPEGQuality(im, verboseFlag)
-            print("quality: {}, sumSqErrors: {}".format(quality, sumSqErrors))
+            quality, sumSqDiffs = computeJPEGQuality(im)
+            print("quality: {}, sumSqDiffs: {}".format(quality, sumSqDiffs))
 
 
 if __name__ == "__main__":
