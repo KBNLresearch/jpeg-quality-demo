b0b248a9cb24eda16b1877f7ae0ac35434d2ae62
bitsgalore <johan.vanderknijff@kb.nl>
2024-10-28 15:33:01 +0100

    added scatter plot script

--- plot-goodness-fit.py
+++ plot-goodness-fit.py
@@ -4,11 +4,14 @@ Write CSV file with luminance, chrominance values from quantization tables,
 as well as corresponding values from closest "standard" tables.
 """
 
-import math
 import os
+import math
 import argparse
 import csv
 from PIL import Image
+import pandas as pd
+from matplotlib import pylab
+from matplotlib import pyplot as plt
 
 def parseCommandLine():
     """Parse command line"""
@@ -25,7 +28,7 @@ def parseCommandLine():
 
 
 def computeJPEGQuality(image):
-    """Returns JPEG quality based on best correspondence between image
+    """Estimates JPEG quality based on best correspondence between image
     quantization tables and standard tables from the JPEG ISO standard.
     
     The image quantization tables are compared against standard quantization
@@ -61,14 +64,21 @@ def computeJPEGQuality(image):
     qdict = image.quantization
     noTables = len(qdict)
 
-    # Get bit depth of quantization tables by checking for any values
-    # greater than 255
+    # Default quantization table bit depth
     qBitDepth = 8
+
     if max(qdict[0]) > 255:
+        # Any values greater than 255 indicate bir depth 16 
         qBitDepth = 16
     if noTables >= 2:
         if max(qdict[1]) > 255:
             qBitDepth = 16
+
+    # Calculate mean of all value in quantization tables
+    Tsum = sum(qdict[0])
+    if noTables >= 2:
+        Tsum += sum(qdict[1])
+    Tmean = Tsum / (noTables*64)
 
     # List for storing squared error values
     errors = []
@@ -76,6 +86,9 @@ def computeJPEGQuality(image):
     ## TEST
     qTablesStandard = []
     ## TEST
+
+    # List for storing Nash–Sutcliffe Efficiency values
+    nseVals = []
 
     # Iterate over all quality levels
     for i in range(100):
@@ -92,10 +105,13 @@ def computeJPEGQuality(image):
         chromStandard = []
         ## TEST
 
-        # Initialize sum of squared errors, which is used to characterize
-        # agreement between image q tables and standard q tables for each
-        # quality level
+        # Initialize sum of squared differences between image quantization values
+        # and corresponding values from standard q tables for this quality level
         sumSqErrors = 0
+
+        # Initialize sum of squared differences between image quantization values
+        # and mean image quantization value (needed to calculate Nash Efficiency)
+        sumSqMean = 0
 
         # Iterate over all values in quantization tables for this quality
         for j in range(64):
@@ -112,6 +128,9 @@ def computeJPEGQuality(image):
             # image table value
             sumSqErrors += (qdict[0][j] - Tslum)**2
 
+            # Sum of luminance and chrominance values          
+            Tcombi = qdict[0][j]
+
             if noTables >= 2:
                 # Compute standard chrominance table value from scaling factor
                 # (Eq 2 in Kornblum, 2008)
@@ -126,9 +145,20 @@ def computeJPEGQuality(image):
                 # image table value
                 sumSqErrors  += (qdict[1][j] - Tschrom)**2
 
+                # Update sum of luminance and chrominance values
+                Tcombi += qdict[1][j]
+   
+            # Update sumSqMMean
+            sumSqMean += (Tcombi - Tmean)**2
+
             j += 1
 
+        # Calculate Nash-Sutcliffe Effiency
+        nse = 1 - sumSqErrors/sumSqMean
+
+        # Add calculated statistics to lists
         errors.append(sumSqErrors)
+        nseVals.append(nse)
         ## TEST
         qTablesStandard.append([lumStandard, chromStandard])
         ## TEST
@@ -142,38 +172,57 @@ def computeJPEGQuality(image):
     # quantization tables. Any other value means non-standard tables were
     # used, and quality estimate is an approximation
     sumSqErrors = min(errors)
-    # Compute corresponding root mean squared error (easier to interpret)
+    # Compute corresponding root mean squared error
     rmsError = round(math.sqrt(sumSqErrors / (noTables * 64)), 3)
+    nse = round(max(nseVals), 3)
+    return qualityEst, rmsError, nse, qTablesStandard[errors.index(min(errors))]
 
-    return qualityEst, rmsError, qTablesStandard[errors.index(min(errors))]
 
 def main():
     args = parseCommandLine()
     myJPEG =  args.JPEGIn
 
-    listOut = [["lum", "lum_s", "chrom", "chrom_s"]]
+    fileName = os.path.basename(myJPEG)
+    baseName = os.path.splitext(fileName)[0]
+
+    listOut = []
 
     with open(myJPEG, 'rb') as fIn:
         im = Image.open(fIn)
         im.load()
-        quality, rmse, qTablesStandard = computeJPEGQuality(im)
+        quality, rmse, nse, qTablesStandard = computeJPEGQuality(im)
         qtables = im.quantization
         noTables = len(qtables)
         for i in range(len(qtables[0])):
             lum = qtables[0][i]
             lum_s = qTablesStandard[0][i]
+            listOut.append([lum, lum_s])
             if noTables >= 2:
                 chrom = qtables[1][i]
                 chrom_s = qTablesStandard[1][i]
-            else:
-                chrom = -1
-                chrom_s = -1
-            listOut.append([lum, lum_s, chrom, chrom_s])
+                listOut.append([chrom, chrom_s])
 
-    with open("qtables.csv", "w", encoding="UTF-8") as fp:
+    # Convert list to Pandas dataframe
+    df = pd.DataFrame(listOut, columns=["T", "Ts"])
+
+    # Maximum Ts value (used for plot)
+    TsMax = df.max(axis=0)['Ts']
+
+    # Create scatter plot of actual vs standard quantization values
+    myPlot = df.plot.scatter(x = 'T', y = 'Ts', s = 20, color = 'b')
+    # Add 1:1 line
+    myPlot.axline([0, 0], [1, 1], linewidth=1, linestyle='dashed', color = 'r')
+    # Add text
+    myPlot.text(0, 0.8*TsMax, f'{fileName}\nQuality = {quality}%\nRMSE = {rmse}\nNSE = {nse}')
+    fig = myPlot.get_figure()
+    fig.savefig(f'{baseName}-scatter.png', dpi=150)
+
+    """
+    with open(f'{baseName}-qvalues.csv', "w", encoding="UTF-8") as fp:
         writer = csv.writer(fp, delimiter=",")
         for row in listOut:
             writer.writerow(row)
+    """
 
 if __name__ == "__main__":
     main()
